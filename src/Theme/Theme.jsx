f563f7067b86ad3ebdf2014d706dd4844216104c
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 <mohammad.mohamadi0101@gmail.com>
2021-12-06 11:27:40 +0330

    theme

---
+++ src/Theme/Theme.jsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+import { faIR } from "@mui/material/locale";
+
+const theme = createTheme(
+  {
+    direction: "rtl",
+  },
+  faIR
+);
+
+export default theme;
