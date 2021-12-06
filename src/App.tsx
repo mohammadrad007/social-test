bebc2a0d0a064a20134f7c6b1489270ca2f4a655
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 <mohammad.mohamadi0101@gmail.com>
2021-12-06 11:29:28 +0330

    project-config

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div className="App-body"></div>
     </div>
   );
-}
+};
 
 export default App;
